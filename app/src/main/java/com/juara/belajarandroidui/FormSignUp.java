5dc0ad6a1f51f9e018bf7512fddee3a510a79133
dewabrata <dewabrata@gmail.com>
2020-02-05 14:27:07 +0700

    penambahan activity SignUp

---
+++ app/src/main/java/com/juara/belajarandroidui/FormSignUp.java
@@ -0,0 +1,14 @@
+package com.juara.belajarandroidui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_sign_up);
+    }
+}
